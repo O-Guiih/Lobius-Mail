55d611901d772e674e1e3acfa9c33588bcfacbd0
João <deandejoao5@gmail.com>
2026-09-17 18:47:39 -0300

    Configuração base do Flask e CORS

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 from routes.status import status_bp
 
 
 app = Flask(__name__)
+
+CORS(app, origins=["http://localhost:5173"])
 
 app.register_blueprint(status_bp)
 
